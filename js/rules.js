cbd000010259cc3d182e90b00b9f5f8d0e727388
Justin Duong <jduong@umich.edu>
2022-11-16 19:47:08 -0500

    Finished responsive design for tablet and desktop view.

--- js/rules.js
+++ js/rules.js
@@ -1,13 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
   const divRulesModal = document.querySelector('.rules-modal');
+  const bgOverlay = document.getElementById('bg-overlay');
   const btnRules = document.querySelector('.div-rules > button');
   const btnCloseModal = document.getElementById('btn-modal-close');
 
   btnRules.addEventListener('click', () => {
     divRulesModal.classList.remove('hide');
+
+    if (window.innerWidth >= 1200) {
+      bgOverlay.classList.remove('hide');
+    }
   });
 
   btnCloseModal.addEventListener('click', () => {
     divRulesModal.classList.add('hide');
+
+    if (window.innerWidth >= 1200) {
+      bgOverlay.classList.add('hide');
+    }
   });
+
+  window.addEventListener('resize', () => {
+    if (window.innerWidth >= 1200 && btnRules.classList.contains('hide')) {
+      bgOverlay.classList.add('hide');
+    }
+  })
 });
